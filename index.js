e070b61192b46ced4681a85b27fff3177cc72933
Noah King <noahking123@yahoo.com>
2022-08-09 08:43:09 -0400

    commit tree ef7bc1481c36a8967e3324f2595e179216018fec

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity1 = 'go to the office') {
+    return `This Monday, I will ${activity1}.`;
+}
+
+function wrapAdjective(highlight = '*', msg = 'You are') {
+   const innerFunction = function(param1 = 'special'){
+    return `${msg} ${highlight}${param1}${highlight}!`;
+    };
+    return innerFunction;
+}
+wrapAdjective('%')('a dedicated programmer');
